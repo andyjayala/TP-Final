1d14eab73da18d03d8f0a0af4a1772dc691588d6
Estefania <emlavalos92@gmail.com>
2025-06-08 10:36:22 -0300

    Agrego parte 3: suma de todos los elementos de una lista con bucle, recursividad y sum

--- TP-Final-Andrea_y_Estefania.py
+++ TP-Final-Andrea_y_Estefania.py
@@ -58,34 +58,77 @@ import time
 lista_pedidos = ["harina", "arroz", "fideos", "azucar", "sal", "arvejas","harina", "arroz", "fideos", "azucar", "sal", "arvejas"]*1000
 
 # start_reversed= time.time()
-def invertir_orden_reversed(lista_pedidos):
-    nueva_lista = list(reversed(lista_pedidos))
-    return nueva_lista
+# def invertir_orden_reversed(lista_pedidos):
+#     nueva_lista = list(reversed(lista_pedidos))
+#     return nueva_lista
 # end_reversed = time.time()
 
 # start_slicing= time.time()
-def invertir_orden_slicing(lista_pedidos):
-    return lista_pedidos[::-1]
+# def invertir_orden_slicing(lista_pedidos):
+#     return lista_pedidos[::-1]
 # end_slicing = time.time()
 
 # start_bucle= time.time()
-nueva_lista=[]
-def invertir_con_bucle(lista_pedidos):
-    for i in range (len(lista_pedidos)-1, -1, -1):
-        nueva_lista.append(lista_pedidos[i])
-    return nueva_lista
+# nueva_lista=[]
+# def invertir_con_bucle(lista_pedidos):
+#     for i in range (len(lista_pedidos)-1, -1, -1):
+#         nueva_lista.append(lista_pedidos[i])
+#     return nueva_lista
 # end_bucle = time.time()
 
-start_reversed= time.time()
-invertir_orden_reversed(lista_pedidos)
-end_reversed = time.time()
+# start_reversed= time.time()
+# invertir_orden_reversed(lista_pedidos)
+# end_reversed = time.time()
 
-start_slicing= time.time()
-invertir_orden_slicing(lista_pedidos)
-end_slicing = time.time()
+# start_slicing= time.time()
+# invertir_orden_slicing(lista_pedidos)
+# end_slicing = time.time()
 
-start_bucle= time.time()
-invertir_con_bucle(lista_pedidos)
-end_bucle = time.time()
+# start_bucle= time.time()
+# invertir_con_bucle(lista_pedidos)
+# end_bucle = time.time()
 
-print(f"Código con reversed: {end_reversed-start_reversed:.20f}, código con slicing: {end_slicing-start_slicing:.20f} y código con bucle: {end_bucle-start_bucle:.20f}")
+# print(f"Código con reversed: {end_reversed-start_reversed:.20f}, código con slicing: {end_slicing-start_slicing:.20f} y código con bucle: {end_bucle-start_bucle:.20f}")
+
+# Parte 2: Calcular la suma de productos vendidos
+# Problema: Cada pedido tiene una cantidad de productos. Clara quiere saber cuántos productos se vendieron en total.
+# Objetivo: Sumar todas las cantidades.
+# Comparación: Usar un for para ir sumando vs usar sum(lista) vs recursividad
+# Resultado:
+# Analogía: Ir sumando uno por uno con la calculadora (for) vs. dejar que una caja registradora lo haga automáticamente (sum()).
+
+lista_cantidad_productos = [1234, 5678, 90, 35000, 950] * 10
+
+
+def suma_bucle(lista_cantidad_productos):
+    suma = 0
+    for cantidad in lista_cantidad_productos:
+        suma += cantidad
+    return suma
+
+start_suma_bucle = time.time()
+suma_bucle(lista_cantidad_productos)
+end_suma_bucle = time.time()
+
+print(suma_bucle(lista_cantidad_productos), f"El tiempo con bucle es: {end_suma_bucle - start_suma_bucle: .6f}")
+
+def suma_con_sum(lista_cantidad_productos):
+    return sum(lista_cantidad_productos)
+
+start_suma_sum = time.time()
+suma_con_sum(lista_cantidad_productos)
+end_suma_sum= time.time()
+
+print(suma_con_sum(lista_cantidad_productos), f"El tiempo con sum es: {end_suma_sum - start_suma_sum: .6f}")
+
+def suma_con_recursividad(lista_cantidad_productos):
+    if len(lista_cantidad_productos) == 0:
+        return 0
+    else:
+        return lista_cantidad_productos[0] + suma_con_recursividad(lista_cantidad_productos[1:])
+    
+start_suma_recursion = time.time()
+suma_con_recursividad(lista_cantidad_productos)
+end_suma_recursion = time.time()
+
+print(suma_con_recursividad(lista_cantidad_productos), f"El tiempo con recursividad es: {end_suma_recursion - start_suma_recursion: .6f}")
